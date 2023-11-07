25a9b2717da1bffe4776152355981c76eb9578e2
Kaguyashi <139529100+Kaguyashi@users.noreply.github.com>
2023-11-07 11:36:17 -0300

    Update import csv.py

--- import csv.py
+++ import csv.py
@@ -7,24 +7,28 @@ def split_lines(input_file, output_file):
         writer = csv.writer(outputcsv, delimiter=';')
 
         for row in reader:
+
             for i, cell in enumerate(row):
                 parts = cell.split('|')
                 if len(parts) > 1:
                     for j in range(len(parts) - 1):
-                        # Append the split information to the current cell
-                        row[i] = parts[j]
-                        # Write the current row with the added information
-                        writer.writerow(row)
-                    # Update the current cell with the remaining part
+                        new_row = row.copy()  # Create a copy of the original row
+                        new_row[i] = parts[j]  # Update the cell in the copy with the split part
+                        new_rows.append(new_row)  # Add the new row to the temporary list
+
+                    # Update the cell in the original row with the remaining part
                     row[i] = parts[-1]
 
-            # Write the current row after all the splits
+            # Write the new rows generated during splitting
+            for new_row in new_rows:
+                writer.writerow(new_row)
+
+            # Write the original row after all the splits
             writer.writerow(row)
 
 # Input and output CSV file names
-input_file = 'o.csv'
-output_file = 'setembro1000.csv'
+input_file = '1.csv'
+output_file = 'september1000.csv'
 
 # Call the function to split the data and write the result to the new file
 split_lines(input_file, output_file)
-
